c0888db44bc133130f230291c7a35113020d3515
tvasuthevan123@gmail.com <45975730+tvasuthevan123@users.noreply.github.com>
2023-04-30 19:19:34 +0100

    List view sorting + over due colouring

--- lib/list_view.dart
+++ lib/list_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:timescape/entry_form.dart';
 import 'package:timescape/task_tile.dart';
-import 'dart:math';
 
 import 'entry_manager.dart';
 
@@ -10,8 +8,8 @@ class EntryListView extends StatelessWidget {
   final EntryType entryType;
   const EntryListView({super.key, required this.entryType});
 
-  TaskTileColors getTileColors(EntryType type) {
-    switch (type) {
+  TaskTileColors getTileColors(Entry entry) {
+    switch (entry.type) {
       case EntryType.reminder:
         return const TaskTileColors(
           tileColor: Color.fromRGBO(11, 213, 156, 1),
@@ -27,6 +25,15 @@ class EntryListView extends StatelessWidget {
           innerTileFontColor: Colors.white,
         );
       default: // For task type
+        if ((entry as Task).deadline.difference(DateTime.now()).inMilliseconds <
+            0) {
+          return const TaskTileColors(
+            tileColor: Color.fromRGBO(220, 53, 69, 1), // red
+            tileBorderColor: Color.fromRGBO(123, 32, 47, 1), // dark red
+            innerTileColor: Color.fromRGBO(255, 204, 204, 1), // light red
+            innerTileFontColor: Color.fromRGBO(123, 32, 47, 1), // dark red
+          );
+        }
         return const TaskTileColors(
           tileColor: Color.fromRGBO(0, 78, 82, 1),
           tileBorderColor: Color.fromRGBO(0, 39, 41, 1),
@@ -42,15 +49,20 @@ class EntryListView extends StatelessWidget {
       builder: (context, itemManager, child) {
         final items = itemManager.entries.values
             .where((entry) => entry.type == entryType)
-            .toList()
-            .reversed
             .toList();
-        final itemKeys = itemManager.entries.keys
-            .where((key) => itemManager.entries[key]!.type == entryType)
-            .toList()
-            .reversed
-            .toList();
-        final colors = getTileColors(entryType);
+
+        items.sort((a, b) {
+          switch (a.type) {
+            case EntryType.task:
+              return (a as Task).deadline.compareTo((b as Task).deadline);
+            case EntryType.reminder:
+              return (a as Reminder)
+                  .dateTime
+                  .compareTo((b as Reminder).dateTime);
+            case EntryType.event:
+              return -1;
+          }
+        });
         return Stack(
           children: [
             ListView.builder(
@@ -58,7 +70,7 @@ class EntryListView extends StatelessWidget {
               itemBuilder: (context, index) {
                 final item = items[index];
                 return TaskTile(
-                    key: Key(itemKeys[index]), item: item, colors: colors);
+                    key: Key(item.id), item: item, colors: getTileColors(item));
               },
             ),
           ],
